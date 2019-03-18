3a48da9e745040a3a37009ae2c1c1d6e4e5b6a73
rangga-putra <rangga.putra@quipper.com>
2019-03-18 16:42:38 +0700

    add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js'
+  },
+  devServer: {
+    inline: true,
+    port: 8080
+  }
+}
